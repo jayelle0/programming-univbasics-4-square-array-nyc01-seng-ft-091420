938a71d4e29be5f9b75d76c201139327692b6b1d
Jacqueline <lojacqueline2@gmail.com>
2020-09-02 02:19:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,8 @@
 # require 'pry'
 def square_array(array)
 counter= 0
- while counter < array.length do
-   puts array[counter]**2
-  counter += 1
- end
+ while count < array.length do
+    array[count] = array[count]**2
+    count += 1
  end
  
